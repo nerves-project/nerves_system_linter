a0beb58ce55b7b7d1aa2812136728ee3951fe3bc
Connor Rigby <connor@farmbot.io>
2017-10-19 08:16:31 -0700

    Prepare for hex publish

--- mix.exs
+++ mix.exs
@@ -6,21 +6,30 @@ defmodule NervesSystemLinter.Mixfile do
       app: :nerves_system_linter,
       version: "0.1.0",
       elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      package: package(),
+      description: description(),
+      start_permanent: Mix.env() == :prod,
+      deps: []
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
+  defp description do
+    """
+    Nerves System Linter - Lint Nervees Defconfigs.
+    """
+  end
+
+  defp package do
     [
+      maintainers: ["Connor Rigby"],
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/nerves-project/nerves_system_linter"}
     ]
   end
 end
