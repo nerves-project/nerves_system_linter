882c87414e37b597444a7d3ce5185b0a0e61967e
Frank Hunleth <fhunleth@troodon-software.com>
2017-11-30 11:55:51 -0500

    Error if systemd or busybox init, etc are enabled

---
+++ rules/init_none.ex
@@ -0,0 +1,7 @@
+defmodule Nerves.System.Linter.Rule.InitNone do
+  use Nerves.System.Linter.Rule
+  ensure_bool("BR2_INIT_NONE", true)
+  # Using systemd or busybox init isn't supported. Nerves expects to use
+  # erlinit.
+  evaluate()
+end
