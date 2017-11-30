125afdb277da5be0e1af39d08cf551252713371d
Frank Hunleth <fhunleth@troodon-software.com>
2017-11-30 11:56:21 -0500

    Need to be able to format ext4 partitions
    Just issue a warning, since it's possible that someone doesn't use ext4.

---
+++ rules/e2fsprogs.ex
@@ -0,0 +1,8 @@
+defmodule Nerves.System.Linter.Rule.E2fsprogs do
+  use Nerves.System.Linter.Rule
+  ensure_package("BR2_PACKAGE_E2FSPROGS", warn: true)
+  # e2fsprogs is used to format the application partition since most Nerves
+  # systems use ext4. If you're not using an application partition or not using
+  # ext4, then feel free to not enable this.
+  evaluate()
+end
