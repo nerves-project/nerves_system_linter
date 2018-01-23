ce955a16127976c611776b87aa0971a06a41f329
connor rigby <connor@farmbot.io>
2018-01-23 09:21:50 -0800

    use BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC.

--- rules/generate_locale.ex
+++ rules/generate_locale.ex
@@ -2,13 +2,13 @@ defmodule Nerves.System.Linter.Rule.GenerateLocale do
   use Nerves.System.Linter.Rule
 
   ensure_value("BR2_GENERATE_LOCALE", "en_US.UTF-8", [
-    message: "BR2_GENERATE_LOCALE should be en_US.UTF-8 if BR2_TOOLCHAIN_USES_GLIBC is disabeld.",
-    if: [{"BR2_TOOLCHAIN_USES_GLIBC", false}]
+    message: "BR2_GENERATE_LOCALE should be en_US.UTF-8 if BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC is enabled.",
+    if: [{"BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC", true}]
   ])
 
   ensure_value("BR2_GENERATE_LOCALE", false, [
-    message: "BR2_GENERATE_LOCALE should be disabled if BR2_TOOLCHAIN_USES_GLIBC is enabled",
-    if: [{"BR2_TOOLCHAIN_USES_GLIBC", true}]
+    message: "BR2_GENERATE_LOCALE should be disabled if BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC is disabled",
+    if: [{"BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC", false}]
   ])
 
   evaluate()
