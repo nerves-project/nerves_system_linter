1cef0ac92412a486dd1f2be850106d73c9178b1a
Frank Hunleth <fhunleth@troodon-software.com>
2018-01-25 09:46:30 -0500

    Update generate locale comment; remove non-glibc test
    Remove the non-glibc test since the glibc check isn't perfect. I.e., if
    a user selects a Buildroot-generated toolchain, they could still have
    glibc and still need BR2_GENERATE_LOCALE. The linter will miss this, but
    at least it won't give a false error.

--- rules/generate_locale.ex
+++ rules/generate_locale.ex
@@ -2,13 +2,8 @@ defmodule Nerves.System.Linter.Rule.GenerateLocale do
   use Nerves.System.Linter.Rule
 
   ensure_value("BR2_GENERATE_LOCALE", "en_US.UTF-8", [
-    message: "BR2_GENERATE_LOCALE should be en_US.UTF-8 if BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC is enabled.",
+    message: "When using glibc, make sure that BR2_GENERATE_LOCALE=en_US.UTF-8.",
     if: [{"BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC", true}]
-  ])
-
-  ensure_value("BR2_GENERATE_LOCALE", false, [
-    message: "BR2_GENERATE_LOCALE should be disabled if BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC is disabled",
-    if: [{"BR2_TOOLCHAIN_EXTERNAL_CUSTOM_GLIBC", false}]
   ])
 
   evaluate()
